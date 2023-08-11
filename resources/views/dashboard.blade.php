eb377fba9f87acd3ab33eea50f23b50c36e4cc82
shang yang <yangshang910215@gmail.com>
2023-08-11 11:39:57 -0400

    styled

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12 dashboard-bg ">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
             <div class="p-6 text-gray-900">
-                {{ __("You're logged in!") }}
+                {{ __("Hello, ") }} {{ Auth::user()->name }} {{ __(".You're logged in!") }}
                 <h2 class="text-lg font-semibold mt-4">Weather in Ottawa</h2>
                 <p class="mt-2">Temperature: {{ round($weatherData['main']['temp'] - 273.15, 2)}} °C</p>
                 <p>Humidity: {{ $weatherData['main']['humidity'] }}%</p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="{{ asset('resources/css/dashboard-bg.css') }}" rel="stylesheet">
 
 
     <!-- Scripts -->
